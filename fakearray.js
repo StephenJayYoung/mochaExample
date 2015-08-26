6ca12df3f6a15184cab2c0fad8622b4b1be7827b
Stephen Young <stephenjayyoung@gmail.com>
2015-08-26 11:41:25 -0700

    added chai testing suite

--- fakearray.js
+++ fakearray.js
@@ -1,14 +1,27 @@
-module.exports = {
-  length: 0,
-  pop: function() {
-    var returnValue = this[this.length - 1];
-    this[this.length-1] = undefined;
-    this.length--;
-    return returnValue;
-  },
-  push: function(input) {
-    this[this.length] = input;
-    this.length++;
-  }
-  // more fake-array methods here...
+// module.exports = {
+//   length: 0,
+//   pop: function() {
+//     var returnValue = this[this.length - 1];
+//     this[this.length-1] = undefined;
+//     this.length--;
+//     return returnValue;
+//   },
+//   push: function(input) {
+//     this[this.length] = input;
+//     this.length++;
+//   }
+//   // more fake-array methods here...
+// };
+
+
+var array = module.exports.array = function(arr) {
+  var arr = [1,2,3],
+    lastElem = arr.pop();
 };
+
+// var arr = [1,2,3],
+//     lastElem = arr.pop();
+
+// module.exports = {
+
+// };
